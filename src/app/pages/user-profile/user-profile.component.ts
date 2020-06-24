4564a5cc783e5ea23806ceb2b49e6d836996481a
Marcuss17 <eduardo.medina@utec.edu.pe>
2020-06-24 11:24:55 -0700

    Sending files

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/core/services/storage-service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+  styleUrls: ['./user-profile.component.scss'],
+  providers: [StorageService]
 })
 export class UserProfileComponent implements OnInit {
+  private identity: any;
+  public birth: string;
+  public email: string;
+  public lastname: string;
+  public name: string;
+  public role: string;
+  public username: string;
 
-  constructor() { }
+  constructor(
+    private storageService: StorageService
+  ) { }
 
   ngOnInit() {
+    this.identity = JSON.parse(this.storageService.getIdentityLocalStorage());
+    console.log(this.identity);
+    this.birth = this.identity.birth;
+    this.email = this.identity.email;
+    this.lastname = this.identity.lastname;
+    this.name = this.identity.name;
+    this.role = this.identity.role;
+    this.username = this.identity.username;
+    
+
   }
+
 
 }
