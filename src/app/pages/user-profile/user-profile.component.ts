58e15cbe3f2276c093aaaa3ee2dbb06e7a5f2cd1
LuchoPonceC <luis.ponce@utec.edu.pe>
2020-06-24 21:32:03 -0700

    covers

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -15,7 +15,10 @@ export class UserProfileComponent implements OnInit {
   public name: string;
   public role: string;
   public username: string;
+  public url_cover: string; 
+  public current_styles: any;
 
+  
   constructor(
     private storageService: StorageService
   ) { }
@@ -29,8 +32,25 @@ export class UserProfileComponent implements OnInit {
     this.name = this.identity.name;
     this.role = this.identity.role;
     this.username = this.identity.username;
-    
+    this.current_styles = {
+      'min-height': '600px',
+      'background-image': this.getUrl(),
+      'background-size': 'cover',
+      'background-position': 'center top',
+    }
 
+
+    
+  }
+
+  getUrl(){
+    switch (this.role) {
+      case 'TEACHER':
+        return 'url(/assets/img/theme/professor_cover.jpg)';
+      case 'STUDENT':
+        return 'url(/assets/img/theme/student_cover.jpg)';
+      
+    }
   }
 
 
