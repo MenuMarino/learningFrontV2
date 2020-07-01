7d3fd46e04b7748acc7c3d1acb5db602b44cbd65
bhn16 <34187868+BHN16@users.noreply.github.com>
2020-07-01 03:25:56 -0500

    show files in progress

--- src/app/pages/files/files.component.ts
+++ src/app/pages/files/files.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-files',
   templateUrl: './files.component.html',
@@ -8,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilesComponent implements OnInit {
 
+  private currentFile : any = null;
+
   constructor() { }
 
   ngOnInit(): void {
+     
   }
 
+  downloadMaterial() {
+
+  }
+
+  chooseFile(file) {
+    this.currentFile = file;
+  }
+
+  public archivos: any[] = [
+    {
+      name: "JSJSJ.pdf",
+      peso: 123,
+      ruta: '../../../assets/pdfs/pdf1.pdf'
+    },
+    {
+      name: "JS.pdf",
+      peso: 123,
+      ruta: '../../../assets/pdfs/pdf2.pdf'
+    },
+    {
+      name: "ll.pdf",
+      peso: 123, 
+      ruta: '../../../assets/pdfs/pdf3.pdf'
+    }
+  ]
 }
