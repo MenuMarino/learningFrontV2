7bd1319f17c89fa62055b44010657807b21dad91
MenuMarino <benjamin.diaz@utec.edu.pe>
2020-07-22 18:45:02 -0500

    Chau curar

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-heroku'));
+app.use(express.static('./dist/angular-heroku'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/angular-heroku/index.html'));
+app.get(’/*’, function(req, res) {
+  res.sendFile(’src/index.html’, {root: 'dist/angular-heroku/’}
+);
 });
 
 // Start the app by listening on the default Heroku port
