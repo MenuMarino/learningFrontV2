5e326606a4d8af9cf8e52137767865222a9175d6
MenuMarino <benjamin.diaz@utec.edu.pe>
2020-07-22 20:30:20 -0500

    Deploy a heroku

--- src/app/core/services/common-services.ts
+++ src/app/core/services/common-services.ts
@@ -4,5 +4,5 @@ import { Injectable, HostListener } from "@angular/core";
   providedIn: "root",
 })
 export class CommonService {
-  baseUrl: string = "https://frozen-castle-49375.herokuapp.com";
+  baseUrl: string = "https://learningperubd.herokuapp.com";
 }
